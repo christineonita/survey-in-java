85f840036780494a1ee6a6ce8581debd17f4ab5d
onitachristine <onitachristine@gmail.com>
2022-10-24 23:15:42 -0400

    commented out some stuff

--- src/survey/Essay.java
+++ src/survey/Essay.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class Essay extends Question implements Serializable { // - short answer will hava character limit set by the creator
     private static final long serialVersionUID = -1175125181899751223L;
     protected String userAnswer;
-    protected int[] essayResponses;
+    protected String[] essayResponses;
     private int numberOfResponsesRequired;
 
     @Override
@@ -25,12 +25,13 @@ public class Essay extends Question implements Serializable { // - short answer 
         //String ans = UserInput.getString();
         //userResponse = ans;
         //System.out.println(ans);
-        essayResponses = new int[numOfResponses];
+        //essayResponses = new int[numOfResponses];
         for (int p = 0; p < numOfResponses; p++) {
             Display.displayString("Response #" + (p + 1));
             String ans = UserInput.getString();
             userAnswer = userAnswer + ans + "\n";
             userResponse = userAnswer;
+            //essayResponses[p] = ans;
         }
     }
 
