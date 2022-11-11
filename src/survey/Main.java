a55228a5c010ae5662ee52ff73a9df3854571a35
Christine Onita <79072825+christineonita@users.noreply.github.com>
2022-11-10 23:34:59 -0500

    added todo notes

--- src/survey/Main.java
+++ src/survey/Main.java
@@ -1,7 +1,12 @@
 package survey;
+
 public class Main {
     public static void main(String[] args) {
         Menus menus = new Menus();
         menus.mainMenu();
     }
+
+
+    // TODO - (-15) Still missing response correct answer class, vital to grading.
+    // TODO - (-7.5) You need some more functions in Test, like modify, take as that functionality will differ from Survey due to the added requirements about correct answers.
 }
