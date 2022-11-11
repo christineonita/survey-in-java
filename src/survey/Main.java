a8829965e42dce53ad390802e10f12835fb7ec73
Christine Onita <79072825+christineonita@users.noreply.github.com>
2022-11-10 23:58:03 -0500

    added todo notes

--- src/survey/Main.java
+++ src/survey/Main.java
@@ -5,8 +5,7 @@ public class Main {
         Menus menus = new Menus();
         menus.mainMenu();
     }
-
-
+    // todo - remember to implement serializable
     // TODO - (-15) Still missing response correct answer class, vital to grading.
     // TODO - (-7.5) You need some more functions in Test, like modify, take as that functionality will differ from Survey due to the added requirements about correct answers.
 }
