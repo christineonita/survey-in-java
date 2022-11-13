bd165bfd1405eead06c13660a3acba9034837ed7
Christine Onita <79072825+christineonita@users.noreply.github.com>
2022-11-13 03:49:18 -0500

    added notes

--- src/survey/Main.java
+++ src/survey/Main.java
@@ -6,6 +6,8 @@ public class Main {
         menus.mainMenu();
     }
     // todo - remember to implement serializable
-    // TODO - (-15) Still missing response correct answer class, vital to grading.
+    // todo - for mult choice test, sort responses - alphabetically? - (both taker and creator) before comparing for grading
+    // todo - (not sure about this) - valid date response should not be after current date - but what if question asks for a future date? - hw2b feedback said valid date took invalid responses?
+    // TODO - (-15) Still missing response correct answer class, vital to grading. - i think ive done this right
     // TODO - (-7.5) You need some more functions in Test, like modify, take as that functionality will differ from Survey due to the added requirements about correct answers.
 }
