d3f12fae371eaa41866154aab79a6c50665d3642
Christine Onita <79072825+christineonita@users.noreply.github.com>
2022-11-18 00:02:09 -0500

    added notes

--- src/survey/Main.java
+++ src/survey/Main.java
@@ -6,6 +6,7 @@ public class Main {
         menus.mainMenu();
     }
     // todo - remember to implement serializable
+    // todo - when modifying a question in test, ask user if they want to modify the correct answer for the question
     // todo - when modifying a survey or test delete the old responses (so just delete the corresponding response folder) since they’ll be invalid
     // todo - for mult choice test, sort responses - alphabetically? - (both taker and creator) before comparing for grading
     // todo - (not sure about this) - valid date response should not be after current date - but what if question asks for a future date? - hw2b feedback said valid date took invalid responses?
