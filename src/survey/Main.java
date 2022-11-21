cc9563c0de9986607ee50676defb749f990efbb9
Christine Onita <79072825+christineonita@users.noreply.github.com>
2022-11-20 23:45:40 -0500

    added notes

--- src/survey/Main.java
+++ src/survey/Main.java
@@ -9,7 +9,7 @@ public class Main {
     // todo - replace all souts with display class
     // todo - when modifying a question in test, ask user if they want to modify the correct answer for the question
     // todo - when modifying a survey or test delete the old responses (so just delete the corresponding response folder) since they’ll be invalid
-    // todo - for mult choice test, sort responses - alphabetically? - (both taker and creator) before comparing for grading
+    // todo - for mult choice test, sort responses - alphabetically? - (both taker and creator) before comparing for grading - no need
     // todo - (not sure about this) - valid date response should not be after current date - but what if question asks for a future date? - hw2b feedback said valid date took invalid responses?
     // TODO - (-15) Still missing response correct answer class, vital to grading. - i think ive done this right
     // TODO - (-7.5) You need some more functions in Test, like modify, take as that functionality will differ from Survey due to the added requirements about correct answers.
